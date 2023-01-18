07643604cf9226f45ffb51a1a368a5f4e9d2ffc6
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2023-01-18 14:45:34 +0700

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://harithya.github.io/Textyp-React-MUI"
+  build: {
+    chunkSizeWarningLimit: 10000
+  }
 })
